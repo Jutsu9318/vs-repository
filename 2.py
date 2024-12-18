6aa3e99a9d25ada3486fffef6f0b131984692aa3
Egor <egortretyakov1006@gmail.com>
2024-12-19 00:40:45 +0300

    2.py

--- 2.py
+++ 2.py
@@ -1,2 +1,14 @@
-print("hello")
-print('22')
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+BT = '7554379497:AAH2ZwuRWSwYqkdDDAPiiOqlDgo9HOLPwq0'
+bot = Bot(token = BT)
+dp = Dispatcher()
+def my_start_filter(message: Message) -> bool:
+    return message.text == '/start'
+@dp.message(my_start_filter)
+async def my_start(message: Message):
+    print(message.model_dump_json(indent=4, exclude_none=True))
+    await message.answer(text = 'this is start command')
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
